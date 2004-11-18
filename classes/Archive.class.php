9743aa89795ac05727fc45ac0d58d028557fbc50
tokigun <tokigun>
2004-11-18 14:15:00 +0000

    잠재적인 버그 수정

--- classes/Archive.class.php
+++ classes/Archive.class.php
@@ -24,7 +24,7 @@ class Archive {
   }
 
   function getEntries() {
-    $filenames = Soojung::queryFilenameMatch($this->year . $this->month . "[^.]+[.]entry$");
+    $filenames = Soojung::queryFilenameMatch("^" . $this->year . $this->month . "[^.]+[.]entry$");
     rsort($filenames);
     $entries = array();
     foreach($filenames as $filename) {
