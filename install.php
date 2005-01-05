c3477de63a039d5a8ed558c145d808c8f4cf412c
ddt <ddt>
2005-01-05 13:10:20 +0000

    설치 화면 개선. 필드 입력이 빠졌을 경우 바로 고칠 수 있게 고침. (#300675)

--- install.php
+++ install.php
@@ -25,79 +25,80 @@ if (isset($_POST["name"])) {
       || empty($_POST["url"]) || empty($_POST["password"])
       || empty($_POST["admin"]) || empty($_POST["email"])
       || empty($_POST["perpage"])) {
-    echo "<font color=\"red\">WARNING: Input name, desc, url, email, password</font><br />";
-    die;
+    echo "<font color=\"red\">WARNING: Input blog name, description, url, name, email, password</font><br />";
+  } else {
+    mkdir("contents", 0777);
+    mkdir("contents/upload", 0777);
+
+    mkdir("templates/.cache", 0777);
+    mkdir("templates/.configs", 0777);
+    mkdir("templates/.compile", 0777);
+
+    mkdir("templates/.admin_cache", 0777);
+    mkdir("templates/.admin_configs", 0777);
+    mkdir("templates/.admin_compile", 0777);
+
+    $f = fopen("contents/.info","w");
+    fwrite($f, "1");
+    fclose($f);
+
+    $fb = isset($_POST["fancyurl"]) ? $_POST["fancyurl"] : "off";
+    $nb = isset($_POST["notify"]) ? $_POST["notify"] : "off";
+    Soojung::writeConfigFile($_POST["name"], $_POST["desc"], $_POST["url"], $_POST["perpage"],
+			     $fb, $nb, $_POST["admin"], $_POST["email"],
+			     md5($_POST["password"]));
+
+    $f = fopen(".htaccess", "w");
+    fwrite($f, "RewriteEngine On\n");
+    fwrite($f, "RewriteRule ^(.+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)[.]html$ ");
+    fwrite($f, $_POST["url"] . "/entry.php?blogid=$5\n");
+    fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+) ");
+    fwrite($f, $_POST["url"] . "/index.php?archive=$1$2\n");
+    fwrite($f, "RewriteRule ^([^/.]+)$ ");
+    fwrite($f, $_POST["url"] . "/index.php?category=$1\n");
+    fwrite($f, "RewriteRule ^page/([0-9]+)$ ");
+    fwrite($f, $_POST["url"] . "/index.php?page=$1\n");
+    fclose($f);
+
+    echo "install success. delete install.php file and change the soojung directory permission to 755<br />";
+    echo "<a href=\"index.php\">home</a>";
+    exit();
   }
-
-  mkdir("contents", 0777);
-  mkdir("contents/upload", 0777);
-
-  mkdir("templates/.cache", 0777);
-  mkdir("templates/.configs", 0777);
-  mkdir("templates/.compile", 0777);
-
-  mkdir("templates/.admin_cache", 0777);
-  mkdir("templates/.admin_configs", 0777);
-  mkdir("templates/.admin_compile", 0777);
-
-  $f = fopen("contents/.info","w");
-  fwrite($f, "1");
-  fclose($f);
-
-  $fb = isset($_POST["fancyurl"]) ? $_POST["fancyurl"] : "off";
-  $nb = isset($_POST["notify"]) ? $_POST["notify"] : "off";
-  Soojung::writeConfigFile($_POST["name"], $_POST["desc"], $_POST["url"], $_POST["perpage"],
-		    $fb, $nb, $_POST["admin"], $_POST["email"],
-		    md5($_POST["password"]));
-
-  $f = fopen(".htaccess", "w");
-  fwrite($f, "RewriteEngine On\n");
-  fwrite($f, "RewriteRule ^(.+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)[.]html$ ");
-  fwrite($f, $_POST["url"] . "/entry.php?blogid=$5\n");
-  fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+) ");
-  fwrite($f, $_POST["url"] . "/index.php?archive=$1$2\n");
-  fwrite($f, "RewriteRule ^([^/.]+)$ ");
-  fwrite($f, $_POST["url"] . "/index.php?category=$1\n");
-  fwrite($f, "RewriteRule ^page/([0-9]+)$ ");
-  fwrite($f, $_POST["url"] . "/index.php?page=$1\n");
-  fclose($f);
-
-  echo "install success. delete install.php file and change the soojung directory permission to 755<br />";
-  echo "<a href=\"index.php\">home</a>";
-  exit();
 }
 ?>
+
+<b>* Note:</b> All fields marked with an asterisk (*) are required.<br /><br />
 
 <form action="install.php" method="post">
 <table>
 
 <tr>
-<td>Blog Name:</td>
+<td>* Blog Name:</td>
 <td><input type="text" name="name" size="30" value="<?=isset($_POST['name']) ? $_POST['name'] : ""?>"></td>
 </tr>
 
 <tr>
-<td>Blog Description:</td>
+<td>* Blog Description:</td>
 <td><input type="text" name="desc" size="50" value="<?=isset($_POST['desc']) ? $_POST['desc'] : ""?>"></td>
 </tr>
 
 <tr>
-<td>Blog URL:</td>
+<td>* Blog URL:</td>
 <td><input type="text" name="url" size="50" value="<?=isset($_POST['url']) ? $_POST['url'] : 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF'])?>"></td>
 </tr>
 
 <tr>
-<td>Admin name:</td>
+<td>* Admin name:</td>
 <td><input type="text" name="admin" value="<?=isset($_POST['admin']) ? $_POST['admin'] : ""?>"></td>
 </tr>
 
 <tr>
-<td>Admin email:</td>
+<td>* Admin email:</td>
 <td><input type="text" name="email" value="<?=isset($_POST['email']) ? $_POST['email'] : ""?>"></td>
 </tr>
 
 <tr>
-<td>Admin password:</td>
+<td>* Admin password:</td>
 <td><input type="password" name="password" value="<?=isset($POST['password']) ? $_POST['password'] : ""?>"></td>
 </tr>
 
