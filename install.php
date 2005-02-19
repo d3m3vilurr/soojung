953fd7e2bd5512aa9826535f973d561f7d3ba889
ditto <ditto>
2005-02-19 01:52:04 +0000

    create .htaccess if fancyurl option is on

--- install.php
+++ install.php
@@ -49,19 +49,21 @@ if (isset($_POST["name"])) {
 			     $fb, $nb, $_POST["admin"], $_POST["email"],
 			     md5($_POST["password"]));
 
-    $f = fopen(".htaccess", "w");
-    fwrite($f, "RewriteEngine On\n");
-    fwrite($f, "RewriteRule ^(.+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)[.]html$ ");
-    fwrite($f, $_POST["url"] . "/entry.php?blogid=$5\n");
-    fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+) ");
-    fwrite($f, $_POST["url"] . "/index.php?archive=$1$2\n");
-    fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+)/([0-9]+) ");
-    fwrite($f, $_POST["url"] . "/index.php?archive=$1$2$3\n");
-    fwrite($f, "RewriteRule ^([^/.]+)$ ");
-    fwrite($f, $_POST["url"] . "/index.php?category=$1\n");
-    fwrite($f, "RewriteRule ^page/([0-9]+)$ ");
-    fwrite($f, $_POST["url"] . "/index.php?page=$1\n");
-    fclose($f);
+    if ($fb == "on") {
+      $f = fopen(".htaccess", "w");
+      fwrite($f, "RewriteEngine On\n");
+      fwrite($f, "RewriteRule ^(.+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)[.]html$ ");
+      fwrite($f, $_POST["url"] . "/entry.php?blogid=$5\n");
+      fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+) ");
+      fwrite($f, $_POST["url"] . "/index.php?archive=$1$2\n");
+      fwrite($f, "RewriteRule ^([0-9]+)/([0-9]+)/([0-9]+) ");
+      fwrite($f, $_POST["url"] . "/index.php?archive=$1$2$3\n");
+      fwrite($f, "RewriteRule ^([^/.]+)$ ");
+      fwrite($f, $_POST["url"] . "/index.php?category=$1\n");
+      fwrite($f, "RewriteRule ^page/([0-9]+)$ ");
+      fwrite($f, $_POST["url"] . "/index.php?page=$1\n");
+      fclose($f);
+    }
 
     echo "install success. delete install.php file and change the soojung directory permission to 755<br />";
     echo "<a href=\"index.php\">home</a>";
