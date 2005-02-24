d01922faefa3ebdcf38cef7757061e95e0dd1801
tokigun <tokigun>
2005-02-24 14:20:56 +0000

    #300751: install.php에서 magic_quotes_gpc 설정이 켜져 있을 때 stripslashes하지 않는 버그 잡음

--- install.php
+++ install.php
@@ -1,6 +1,18 @@
 <?php
 include_once("classes/Soojung.class.php");
 include_once("libs/util.php");
+
+if (get_magic_quotes_gpc()) {
+  function stripslashes_deep($value) {
+    $value = is_array($value) ?
+      array_map('stripslashes_deep', $value) :
+      stripslashes($value);
+    return $value;
+  }
+  $_POST = array_map('stripslashes_deep', $_POST);
+  $_GET = array_map('stripslashes_deep', $_GET);
+  $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+}
 ?>
 
 <html>
